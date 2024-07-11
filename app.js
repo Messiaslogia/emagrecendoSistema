b7d4a3b162bc078515193d861665a798089d05be
Messiaslogia <messias.logia@gmail.com>
2024-07-11 13:43:07 -0300

    Ajustes: Adicionando Metas Vendedores

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Configs
+// Configss
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
