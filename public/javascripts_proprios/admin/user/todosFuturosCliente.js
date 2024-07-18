e7f33388d77f65ce216ed3f376edd7ab3a98d232
Messiaslogia <messias.logia@gmail.com>
2024-07-18 11:02:46 -0300

    Ajustes: Futuro Clientes Admin

--- public/javascripts_proprios/admin/user/todosFuturosCliente.js
+++ public/javascripts_proprios/admin/user/todosFuturosCliente.js
@@ -1,9 +1,7 @@
 const container_users = document.querySelector('#Container_Users');
 const urls = "http://localhost:3030/api/todosFuturosClientes";
-const inputsPages = document.querySelectorAll('#pageInput')
 let itensPorPagina = 5;
-let listUsuarios
-
+let listUsuarios;
 
 document.addEventListener('DOMContentLoaded', () => {
     adquirirListsUsers();
@@ -13,16 +11,17 @@ function adquirirListsUsers() {
     axios.get(urls)
         .then(data => {
             listUsuarios = data.data;
+            console.log(listUsuarios);
             setTimeout(() => {
-                displayItens(1, 0)
-            }, [300])
+                displayItens(1)
+            }, 300)
         })
         .catch(err => {
             console.log(err)
         })
 };
 
-function displayItens( page){
+function displayItens(page) {
     let startIndex = (page - 1) * itensPorPagina;
     let endIndex = startIndex + itensPorPagina;
     let arrayPedidos = listUsuarios;
@@ -30,14 +29,14 @@ function displayItens( page){
     let pageItens = reversePedidos.slice(startIndex, endIndex);
 
     // Exibindo os itens
-   container_users.innerHTML = '';
+    container_users.innerHTML = '';
 
     pageItens.map(user => {
         container_users.innerHTML += `
             <li class="list-group-item border-0 d-flex p-4 mb-2 bg-gray-100 border-radius-lg mobile_userlist">
                 <div class="d-flex flex-column">
                     <h5 class="fs-5">${user.nome}</h5>
-                    <h6 class="mb-3 fs-6">Distribuidor</h6>
+                    <h6 class="mb-3 fs-6">${user.nome = 5 ? 'Futuro Cliente' : "null"}</h6>
                     <span class="mb-2 text-xs">E-mail: <span class="text-dark ms-sm-2 font-weight-bold">${user.email}</span></span>
                     <span class="text-xs">Contato: <span class="text-dark ms-sm-2 font-weight-bold">${user.telefone}</span></span>
                     <span class="text-xs">Bairro: <span class="text-dark ms-sm-2 font-weight-bold">${user.bairro}</span></span>
@@ -52,13 +51,13 @@ function displayItens( page){
                 </div>
                 <hr>
             </li>
-        `;                   
+        `;
     })
-    paginas(page, arrayindex);
+    paginas(page);
 };
 
-function paginas(page, array) {
-    const pageCont = Math.ceil(listUsuarios[array].length / itensPorPagina);
+function paginas(page) {
+    const pageCont = Math.ceil(listUsuarios.length / itensPorPagina);
     const containerPagination = document.querySelector('#pag_navigation_input');
 
     containerPagination.innerHTML = '';
@@ -68,32 +67,32 @@ function paginas(page, array) {
     if (pageCont <= maxPagesToShow) {
         for (let i = 1; i <= pageCont; i++) {
             const activeClass = (i === page) ? 'active bg-primary text-light' : '';
-            containerPagination.innerHTML += `<li class="page-item cursor-pointer"><a class="page-link ${activeClass}" onclick="displayItens(${i}, ${array})">${i}</a></li>`;
+            containerPagination.innerHTML += `<li class="page-item cursor-pointer"><a class="page-link ${activeClass}" onclick="displayItens(${i})">${i}</a></li>`;
         }
     } else {
         if (page <= maxPagesToShow - 1) {
             for (let i = 1; i <= maxPagesToShow; i++) {
                 const activeClass = (i === page) ? 'active bg-primary text-light' : '';
-                containerPagination.innerHTML += `<li class="page-item cursor-pointer"><a class="page-link ${activeClass}" onclick="displayItens(${i}, ${array})">${i}</a></li>`;
+                containerPagination.innerHTML += `<li class="page-item cursor-pointer"><a class="page-link ${activeClass}" onclick="displayItens(${i})">${i}</a></li>`;
             }
             containerPagination.innerHTML += `<li class="page-item cursor-pointer"><a class="page-link">...</a></li>`;
-            containerPagination.innerHTML += `<li class="page-item cursor-pointer"><a class="page-link" onclick="displayItens(${pageCont}, ${array})">${pageCont}</a></li>`;
+            containerPagination.innerHTML += `<li class="page-item cursor-pointer"><a class="page-link" onclick="displayItens(${pageCont})">${pageCont}</a></li>`;
         } else if (page >= pageCont - maxPagesToShow + 2) {
-            containerPagination.innerHTML += `<li class="page-item cursor-pointer"><a class="page-link" onclick="displayItens(1, ${array})">1</a></li>`;
+            containerPagination.innerHTML += `<li class="page-item cursor-pointer"><a class="page-link" onclick="displayItens(1)">1</a></li>`;
             containerPagination.innerHTML += `<li class="page-item cursor-pointer"><a class="page-link">...</a></li>`;
             for (let i = pageCont - maxPagesToShow + 1; i <= pageCont; i++) {
                 const activeClass = (i === page) ? 'active bg-primary text-light' : '';
-                containerPagination.innerHTML += `<li class="page-item cursor-pointer"><a class="page-link ${activeClass}" onclick="displayItens(${i}, ${array})">${i}</a></li>`;
+                containerPagination.innerHTML += `<li class="page-item cursor-pointer"><a class="page-link ${activeClass}" onclick="displayItens(${i})">${i}</a></li>`;
             }
         } else {
-            containerPagination.innerHTML += `<li class="page-item cursor-pointer"><a class="page-link" onclick="displayItens(1, ${array})">1</a></li>`;
+            containerPagination.innerHTML += `<li class="page-item cursor-pointer"><a class="page-link" onclick="displayItens(1)">1</a></li>`;
             containerPagination.innerHTML += `<li class="page-item cursor-pointer"><a class="page-link">...</a></li>`;
             for (let i = page - 1; i <= page + 1; i++) {
                 const activeClass = (i === page) ? 'active bg-primary text-light' : '';
-                containerPagination.innerHTML += `<li class="page-item cursor-pointer"><a class="page-link ${activeClass}" onclick="displayItens(${i}, ${array})">${i}</a></li>`;
+                containerPagination.innerHTML += `<li class="page-item cursor-pointer"><a class="page-link ${activeClass}" onclick="displayItens(${i})">${i}</a></li>`;
             }
             containerPagination.innerHTML += `<li class="page-item cursor-pointer"><a class="page-link">...</a></li>`;
-            containerPagination.innerHTML += `<li class="page-item cursor-pointer"><a class="page-link" onclick="displayItens(${pageCont}, ${array})">${pageCont}</a></li>`;
+            containerPagination.innerHTML += `<li class="page-item cursor-pointer"><a class="page-link" onclick="displayItens(${pageCont})">${pageCont}</a></li>`;
         }
     }
 }
