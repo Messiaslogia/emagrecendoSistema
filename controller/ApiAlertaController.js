4a5ee7d8d0589e4d510fb12e2e2e8563c1434763
unknown <gustavo_raniele@hotmail.com>
2024-08-04 15:21:28 -0300

    Ajustes

--- controller/ApiAlertaController.js
+++ controller/ApiAlertaController.js
@@ -1,15 +1,15 @@
 const cache = require('../configs/cache');
 const axios = require('axios');
-const url_base = ''
+const url_base = 'http://localhost:200/alertas'
 
 class Alertas_Controller{
 
     consultandoAlertasAdmin( req, res ){
         const id_admin = cache.get(req.Id_User);
 
-        axios.get(`${url_base}?user=${id_admin}`)
+        axios.get(`${url_base}/alertasAdmin?user=${id_admin}`)
             .then((result) => {
-                res.json(result);
+                res.json(result.data);
             }).catch((err) => {
                 res.json({ status: 511, menssage: 'Erro ao verificar alertas do usuário', error: err });
             });
--- public/javascripts_proprios/alertas/admin/alertasGerais.js
+++ public/javascripts_proprios/alertas/admin/alertasGerais.js
@@ -2,68 +2,54 @@ const url_alertas_admin = "/alertas/alertasGeraisAdmin";
 const id_user = document.querySelector('#Id_User').value;
 const div_dos_alertas = document.querySelector('#DivAlertas')
 
-axios.get(`${url_alertas_admin}?user=${id_user}`)
-    .then((result) => {
-           exibir_alertas(result);
-    }).catch((err) => {
-        
-    });
+document.addEventListener('DOMContentLoaded', () => {
+    axios.get(`${url_alertas_admin}?user=${id_user}`)
+        .then((result) => {
+            exibir_alertas(result.data);
+        }).catch((err) => {
+            console.log(err);
+        });
+})
+
+
 
 function exibir_alertas(array){
-    array.forEach(alerta => {
-        switch (alerta.tema) {
-            // Produtos
-            case '1':
-                div_dos_alertas.innerHTML += `
-                    <li class="mb-2">
-                        <a class="dropdown-item border-radius-md" href="javascript:;">
-                            <div class="d-flex py-1">
-                                <div class="my-auto">
-                                    <span class="material-symbols-outlined">
-                                        inventory_2
-                                    </span>
-                                </div>
-                                <div class="d-flex flex-column justify-content-center">
-                                    <h6 class="text-sm font-weight-normal mb-1">
-                                        <span class="font-weight-bold">O produto: </span> está com estoque baixo!
-                                    </h6>
-                                    <p class="text-xs text-secondary mb-0">
-                                        <span class="material-symbols-outlined me-1">
-                                            schedule
-                                        </span>
-                                        13 minutes ago
-                                    </p>
-                                </div>
-                            </div>
-                        </a>
-                    </li>
-                    `
-                break;
-
-            // Pedidos
-            case '2':
-                
-                break;
-
-            // Usuários
-            case '3':
-                    
-                break;
-
-            // Entregas
-            case '4':
-                        
-                break;
-
-            // Dividas
-            case '5':
-                        
-                break;
-
-        
-            default:
-                break;
+    array.forEach(element => {
+        if(element != null){
+            document.querySelector('#TemNotificacao').style.display = "block";
+            document.querySelector('#SemNotificacao').style.display = "none";
+        }else{
+            console.log(element);
         }
     })
+
+    array[1].forEach(alerta => {
+        div_dos_alertas.innerHTML += `
+            <li class="mb-2">
+                <a class="dropdown-item border-radius-md" href="javascript:;">
+                    <div class="d-flex py-1">
+                        <div class="py-auto">
+                            <div class="border border-5 border-danger rounded rounded-circle bg-danger">
+                                <span class="material-symbols-outlined align-middle" style="margin-left: 1px; color: #000">
+                                    inventory_2
+                                </span>
+                            </div>   
+                        </div>
+                        <div class="d-flex flex-column justify-content-center ms-3">
+                            <h6 class="text-sm font-weight-normal mb-1">
+                                <span class="font-weight-bold">${alerta.mensagem} ${alerta.nome_produto}</span>
+                            </h6>
+                            <p class="text-xs text-secondary mb-0">
+                                <span class="material-symbols-outlined me-1 align-middle" style="font-size: 120%">
+                                    schedule
+                                </span>
+                                13 minutes ago
+                            </p>
+                        </div>
+                    </div>
+                </a>
+            </li>
+        `
+    });
 }
 
